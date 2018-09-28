31d033da258bbaf4a578f95a422754538fd91ac6
devinkapelanski <devinkapelanski@yahoo.com>
2018-09-28 07:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    var cart = [];
+    var cart = 0;
     return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`) + cart;
   }
 }
