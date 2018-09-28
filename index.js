c92623d35445864d5b5233d2875c9757c6c5c011
devinkapelanski <devinkapelanski@yahoo.com>
2018-09-28 07:46:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,9 +57,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    console.log("Sorry, we don't have a credit card on file for you.");
+    return "Sorry, we don't have a credit card on file for you.";
   } else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     cart = [];
   }
 }
